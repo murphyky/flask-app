80fb6e22b244004c4d6a2e041ac4dae754bf5636
murphyky <kyle-murphy89@live.com>
2015-06-14 14:11:50 +0100

    updates

--- myapp/app/views.py
+++ myapp/app/views.py
@@ -3,4 +3,6 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-	return "Hello World!"
+	user = {'nickname':'Miguel'}
+	return render_template('index.html',
+		title='Home',user=user)	
