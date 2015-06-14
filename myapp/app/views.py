2f465e6c23732f00d27791ce9a7aeb5442b84a46
murphyky <kyle-murphy89@live.com>
2015-06-14 14:24:01 +0100

    updates

--- myapp/app/views.py
+++ myapp/app/views.py
@@ -5,5 +5,12 @@ from app import app
 @app.route('/index')
 def index():
 	user = {'nickname':'Miguel'}
+	posts = [
+		{'author': {'nickname': 'John'},
+		'body': 'Beautiful day in Portland!'},
+		{'author': {'nickname': 'Susan'},
+		'body':'The Avengers was shit!'}
+	]
+
 	return render_template('index.html',
-		title='Home',user=user)	
+		title='Home',user=user, posts=posts)	
